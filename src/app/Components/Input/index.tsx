109bddd29b29757b236d09cbc108de43e4345908
Flathin <flathinsees@gmail.com>
2023-06-08 21:20:28 -0300

    Alter return prop

--- src/app/Components/Input/index.tsx
+++ src/app/Components/Input/index.tsx
@@ -1,12 +1,12 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, ReactNode } from "react";
 
 type InputProps = {
-  hasIconleft?: () => void
+  hasIconleft?: () => ReactNode
 } & InputHTMLAttributes<HTMLInputElement>
 
 export function Input({ hasIconleft, ...rest }: InputProps) {
   return (
-    <label for="id" className="flex rounded-2xl px-3 py-2 bg-zinc-100 gap-2 items-center flex-1">
+    <label htmlFor="id" className="flex rounded-2xl px-3 py-2 bg-zinc-100 gap-2 items-center flex-1">
       {hasIconleft && hasIconleft()}
       <input id="id" type="text" {...rest} className="bg-transparent outline-none text-sm" />
     </label>
